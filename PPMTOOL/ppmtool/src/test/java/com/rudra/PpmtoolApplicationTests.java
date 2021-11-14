2aacab51872c6e720234c3fffdbd0c26fdd0cc0c
suniltripathi <suniltripathi.mca07@gmail.com>
2021-11-14 23:04:35 +0530

    First Commit

---
+++ PPMTOOL/ppmtool/src/test/java/com/rudra/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
